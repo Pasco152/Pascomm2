16374654b994364b6e66dfb769b98853e11624af
Pasco <Pasco@163.com>
2020-11-09 09:46:03 +0800

    project - new router and Element-UI

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 导入ele UI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+import router from './router'  // 路由实例对象
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
